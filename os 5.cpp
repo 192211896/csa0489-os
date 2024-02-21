726205e0c423aaa80827e8f1ead53084a716b889
thirupathirao <132388521+192211896@users.noreply.github.com>
2024-02-21 13:14:29 +0530

    Add files via upload

--- os 5.cpp
+++ os 5.cpp
@@ -1,71 +1,46 @@
-#include<stdio.h>
-struct priority_scheduling 
+#include <stdio.h>
+#include <stdlib.h>
+struct Process 
 {
-  char process_name;
-  int burst_time;
-  int waiting_time;
-  int turn_around_time;
-  int priority;
+    int id;
+    int priority;
+    int burst_time;
 };
+void priorityScheduling(struct Process processes[], int n) 
+{
+    for (int i = 0; i < n - 1; i++) 
+	{
+        for (int j = 0; j < n - i - 1; j++) 
+		{
+            if (processes[j].priority < processes[j + 1].priority) 
+			{
+                struct Process temp = processes[j];
+                processes[j] = processes[j + 1];
+                processes[j + 1] = temp;
+            }
+        }
+    }
+    printf("Execution Order:\n");
+    for (int i = 0; i < n; i++) {
+    printf("Process %d (Priority %d, Burst Time %d)\n", processes[i].id, processes[i].priority, processes[i].burst_time);
+    }
+}
 int main() 
 {
-  int number_of_process;
-  int total = 0;
-  struct priority_schedulingtemp_process;
-  int ASCII_number = 65;
-  int position;
-  float average_waiting_time;
-  float average_turnaround_time;
-  printf("Enter the total number of Processes: ");
-  scanf("%d", &number_of_process);
-  struct priority_scheduling process[number_of_process];
-  printf("\nPlease Enter the  Burst Time and Priority of each process:\n");
-  for (int i = 0; i<number_of_process; i++) 
-  {
-   process[i].process_name = (char) ASCII_number;
-   printf("\nEnter the details of the process %c \n", process[i].process_name);
-   printf("Enter the burst time: ");
-   scanf("%d", & process[i].burst_time); 
-   printf("Enter the priority: ");
-   scanf("%d", & process[i].priority);
-   ASCII_number++;
-  }
-  for (int i = 0; i<number_of_process; i++) 
-  {
-    position = i;
-    for (int j = i + 1; j <number_of_process; j++) 
-	{
-      if (process[j].priority> process[position].priority)
-        position = j;
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    struct Process *processes = (struct Process *)malloc(n * sizeof(struct Process));
+   for (int i = 0; i < n; i++)
+	 {
+        processes[i].id = i + 1;
+        printf("Enter priority for process %d: ", i + 1);
+        scanf("%d", &processes[i].priority);
+        printf("Enter burst time for process %d: ", i + 1);
+        scanf("%d", &processes[i].burst_time);
     }
-	temp_process = process[i];
-    process[i] = process[position];
-    process[position] = temp_process;
-  }
-  process[0].waiting_time = 0;
-  for (int i = 1; i<number_of_process; i++) 
-  {
-    process[i].waiting_time = 0;
-    for (int j = 0; j <i; j++) 
-	{
-      process[i].waiting_time += process[j].burst_time;
-    }          
-    total += process[i].waiting_time;
-  }
-  average_waiting_time = (float) total / (float) number_of_process;
-  total = 0;
-  printf("\n\nProcess_name \t Burst Time \t Waiting Time \t  Turnaround Time\n");
-  printf("------------------------------------------------------------\n");
-  for (int i = 0; i<number_of_process; i++) 
-  {
-   process[i].turn_around_time = process[i].burst_time + process[i].waiting_time;
-   total += process[i].turn_around_time;
-   printf("\t  %c \t\t  %d \t\t %d \t\t %d", process[i].process_name, process[i].burst_time, process[i].waiting_time, process[i].turn_around_time);
-   printf("\n-----------------------------------------------------------\n");
-  }
- average_turnaround_time = (float) total / (float) number_of_process;
- printf("\n\n Average Waiting Time : %f", average_waiting_time);
- printf("\n Average Turnaround Time: %f\n", average_turnaround_time);
- return 0;
+    priorityScheduling(processes, n);
+    free(processes);
+    return 0;
 }
 
